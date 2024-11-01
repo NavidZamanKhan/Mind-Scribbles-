548176a136ccaf506ac51f0ccd9936c8d8ce1030
Navid444 <navid166418@gmail.com>
2024-11-01 23:43:10 +0600

    Updated server.py and contact.html, added email.env

--- server.py
+++ server.py
@@ -1,5 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests, datetime
+from dotenv import load_dotenv
+import os, smtplib
+
+load_dotenv("email.env")
+
+OWN_EMAIL = os.getenv("gmail")
+OWN_PASSWORD = os.getenv("password")
+
 
 app = Flask(__name__)
 
@@ -29,6 +37,24 @@ def show_post(index):
         if blog_post["id"] == index:
             requested_post = blog_post
     return render_template("post.html", post=requested_post)
+
+
+@app.route("/form-entry", methods=["GET", "POST"])
+def receive_data():
+    if request.method == "POST":
+        data = request.form
+        data = request.form
+        send_email(data["name"], data["email"], data["phone"], data["message"])
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+def send_email(name, email, phone, message):
+    email_message = f"Subject:New message\n\nName: {name}\nEmail: {email}\nPhone{phone}\nMessage: {message}"
+    with smtplib.SMTP("smtp.gmail.com", 587) as connection:
+        connection.starttls()
+        connection.login(OWN_EMAIL, OWN_PASSWORD)
+        connection.sendmail(OWN_EMAIL, OWN_EMAIL, email_message)
 
 
 if __name__ == "__main__":
